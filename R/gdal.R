c638de082cdd962ff8699d23f9d3c31265e25382
Michael Sumner <mdsumner@gmail.com>
2025-03-06 17:39:55 +1100

    print method

--- R/gdal.R
+++ R/gdal.R
@@ -21,8 +21,26 @@ multidimraster <- function(x, allowed_drivers = character(), open_options = char
 
 #' @export
 print.osgeo.gdal.Dataset <- function(x, ...) {
-  sds <- x$
-  writeLines(.gdal$Info(x))
+  if (!is.null(rg <- x$GetRootGroup())) {
+   cat(sprintf("MultiDimRaster: %s\n",  x$GetDescription()))
+    allarrays<- get_all_array_full_names(x)
+    alldims <- character(0)
+    bad <- integer()
+    for (i in seq_along(allarrays)) {
+      dims <- try(rg$OpenMDArrayFromFullname(allarrays[i])$GetDimensions(), silent = TRUE)
+      if (inherits(dims, "try-error")) {
+        bad <- c(bad, i)
+        next; ## FIXME
+      }
+      alldims <- c(alldims, paste0(unlist(lapply(dims, \(.x) .x$GetSize())), collapse = "x"))
+
+    }
+    allarrays <- allarrays[-bad]
+   nc <- nchar(alldims)
+    writeLines(sprintf("ARRAY: [%s] %s", alldims, allarrays)[order(nc, allarrays,  decreasing = FALSE)])
+  } else {
+    writeLines(.gdal$Info(x))
+  }
 }
 
 ## heinous but this works
@@ -45,7 +63,7 @@ get_all_array_full_names <- function(ds) {
     arrs <- c(arrs, paste0(path, "/", g$GetMDArrayNames()))
   }
 
-  arrs
+  gsub("/$", "", arrs)
 }
 
 
