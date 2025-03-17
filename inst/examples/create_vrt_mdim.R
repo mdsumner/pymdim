3181d7924276e29e56cf51cc8e3595979f38992e
Michael Sumner <mdsumner@gmail.com>
2025-03-17 15:04:02 +1100

    more cleanups and example file

--- inst/examples/create_vrt_mdim.R
+++ inst/examples/create_vrt_mdim.R
@@ -25,7 +25,7 @@ get_dim <- function(x, varname) {
 }
 
 #dsn <- "/vsicurl/https://thredds.nci.org.au/thredds/fileServer/gb6/BRAN/BRAN2020/daily/ocean_salt_1997_04.nc"
-dates <- head(seq(as.Date("1993-01-15"), Sys.Date(), by = "1 month"), 20)
+dates <- seq(as.Date("1993-01-15"), Sys.Date(), by = "1 month")
 
 ## flush all files to start with
 varname <- "salt"
@@ -47,8 +47,8 @@ for (i in seq_along(allfiles)) {
 
 allfiles <- allfiles[ok]
 dsn <- allfiles[1]
-files <- allfiles[-1]
-alldims <- alldims[ok][-1]
+alldims <- alldims[ok]
+dm <- rbind(dim, do.call(rbind, alldims))
 
 #files <- files[1:3]
 library(xml2)
@@ -59,15 +59,14 @@ node0 <- arraynodes[which(unlist(xml_attrs(arraynodes)) == varname)]
 
 sourcenodes <- xml_find_all(node0, "Source")
 firstfile <- xml_text(sourcenodes |> xml_find_all("SourceFilename"))
-dim <- get_dim(firstfile, varname)
-dm <- rbind(dim, do.call(rbind, alldims))
+dim <- dm[1L, ]
 
 incr_idx <- 1 ## the c(30, ...) accumulates
 offset <- dim[incr_idx] - 1
 sn <- sourcenodes[[1]]
 
 
-for (i in seq_along(files)) {
+for (i in 2:length(allfiles)) {
 
   #dim <- get_dim(files[i], varname)
   dim <- alldims[[i]]
@@ -84,8 +83,10 @@ for (i in seq_along(files)) {
 
 timenode <- xml_find_first(x, "//Dimension[@name='Time']")
 xml_attr(timenode, "size") <- sum(dm[,1])
-write_xml(x, tf <- tempfile(fileext = ".vrt"))
-browseURL(tf)
+write_xml(x, "inst/examples/bluelink_ocean_salt.vrt")
+
+#write_xml(x, tf <- tempfile(fileext = ".vrt"))
+#browseURL(tf)
 
 reticulate::py_require("gdal")
 gdal <- reticulate::import("osgeo.gdal")
